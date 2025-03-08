bb362f8a427eb1f8b10c576c32fac1e0a0444dc9
mobasshir <mobasshirkhan9931@gmail.com>
2025-03-08 21:40:45 +0530

    complete data Type

--- ch01/02_datatype.js
+++ ch01/02_datatype.js
@@ -1,2 +1,11 @@
 "use strict"; // treat all JS code as newer version
 //generally inbuild 
+let name = "mobi" //string
+let age = 18      //integer
+let isloggedIn = true //boolean
+let State = null
+// null => standAlone value[empty]
+// undefined => not defined 
+// symbol => unique
+console.log(typeof null); //used to fined dataType
+

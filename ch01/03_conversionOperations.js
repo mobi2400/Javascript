b9d25a5c88466e9100d1ea482ec04a6834ca6c9c
mobasshir <mobasshirkhan9931@gmail.com>
2025-03-08 22:22:44 +0530

    operations

---
+++ ch01/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let score = 33 // number
+let score1 = "33" //string
+console.log(typeof score);
+console.log(typeof score1);
+let ValueInNumber = Number(score1) // modify datatype in num
+/*instead of number u can write any data type in boolean
+1 means true and 0 means false
+when used " " empty strings then it is false
+when used "mobi" it is true*/
+
+console.log(typeof ValueInNumber);

742800acac3f0e16cc76bdfe67d46a25872d8e09
mobasshir <mobasshirkhan9931@gmail.com>
2025-03-13 16:02:37 +0530

    date

---
+++ ch01/08_date.js
@@ -0,0 +1,10 @@
+let MyDate = new Date();// u can add u r own date in format of y,m,d and months start with of 0 in js
+// .now will give tme span in milli second from time set to current time
+console.log(MyDate);
+console.log(MyDate.toString());
+console.log(MyDate.toDateString());
+console.log(MyDate.toISOString());
+console.log(MyDate.toJSON());
+console.log(MyDate.toLocaleDateString());
+// date is object
+

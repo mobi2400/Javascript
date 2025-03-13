d6b295037b8789d79af6e5d136b83fffa2dd6c30
mobasshir <mobasshirkhan9931@gmail.com>
2025-03-14 00:57:07 +0530

    return print differnce

---
+++ ch03/01_function.js
@@ -0,0 +1,8 @@
+function addTwoNumber(num1,num2){
+    return num1 + num2; 
+    console.log(num1 + num2);
+// there is difference between console.log and return. return means to give the value and console is to print the value
+// after return no code is executed 
+}
+
+ 

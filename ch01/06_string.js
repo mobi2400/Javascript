5676f6ce2d968f3128517239f6dec375e770fd79
mobasshir <mobasshirkhan9931@gmail.com>
2025-03-13 14:51:39 +0530

    string manipulation

---
+++ ch01/06_string.js
@@ -0,0 +1,16 @@
+// use `` instead of "" when adding variable 
+const name = "Mobasshir Khan"
+console.log(`hello my name is ${name}`); // ${} use to add a var
+console.log(name.length); 
+console.log(name.toUpperCase()); 
+console.log(name.charAt(3)); 
+console.log(name.indexOf('K'));
+const NewString = name.substring(0,8); // .slice is a better option
+console.log(NewString);
+// .trim remove starting and end space in string
+
+const url = "https://www.youtube.com/";
+console.log( url.replace('youtube.com', 'google.com')); 
+//important manipulation[url manipulation is important]
+
+

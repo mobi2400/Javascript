006415a8e87c45b22736be80bad16437f8e3176a
mobasshir <mobasshirkhan9931@gmail.com>
2025-03-13 15:27:20 +0530

    num and maths

---
+++ ch01/07_num_and_maths.js
@@ -0,0 +1,19 @@
+const score = 420;
+console.log(score);
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString()); // convert to string
+console.log(balance.toFixed(2)); // give precise value, will use in e-commerce website
+// .toPrecision() is also used to round off
+
+
+// +++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.856)); 
+// ceil and floor is also there ceil for top value and floor is for ground value [not mostly used]
+console.log(Math.random()); // gives value always in btw 0 to 1
+
+// there is also min max and many more
+// use mdn doc to know more

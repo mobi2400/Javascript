0e22b3f0bed9235ef70b07e647ad5a4dba8ed6cc
mobasshir <mobasshirkhan9931@gmail.com>
2025-04-02 00:51:53 +0530

    calll back function

---
+++ ch08/01_callBackFunc.js
@@ -0,0 +1,9 @@
+function square(n){
+    return n*n;
+}
+function sumOfSquare(a,b){
+    const val1 = square(a);
+    const val2 = square(b);
+    return val1 + val2
+}
+console.log(sumOfSquare(10,20));

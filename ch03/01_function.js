01d4ad37350dd38ebc2f00f95bb9fd7699922488
mobasshir <mobasshirkhan9931@gmail.com>
2025-04-01 23:40:38 +0530

    function

--- ch03/01_function.js
+++ ch03/01_function.js
@@ -4,5 +4,8 @@ function addTwoNumber(num1,num2){
 // there is difference between console.log and return. return means to give the value and console is to print the value
 // after return no code is executed 
 }
-
- 
+function getLength(str){
+    console.log("original String:",str);
+    console.log("length",str.length);
+}
+getLength("mobasshir")
